43a5fc965c9d03de387da8244234b940fd70f1bb
Ahmed Amine Doudech <ahmedaminedoudech@gmail.com>
2023-08-15 18:53:46 +0100

    add post method

--- examples/app.js
+++ examples/app.js
@@ -1,4 +1,4 @@
-const App = require("../lib/app");
+const App = require("../lib");
 const app = new App();
 
 app.get("/", (req, res) => {
@@ -9,6 +9,11 @@ app.get("/", (req, res) => {
 app.get("/foo", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("bar!\n");
+});
+
+app.post("/code", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("this was a post request.");
 });
 
 app.listen(3000);
